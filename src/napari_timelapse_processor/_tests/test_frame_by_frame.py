ddff331dbfda792ffe3edf1445499b01becac9a8
Johannes Müller <38459088+jo-mueller@users.noreply.github.com>
2024-07-02 07:49:38 +0200

    put `use_dask` argument in correct location

--- src/napari_timelapse_processor/_tests/test_frame_by_frame.py
+++ src/napari_timelapse_processor/_tests/test_frame_by_frame.py
@@ -38,8 +38,8 @@ def test_frame_by_frame_image(create_4D_image):
     assert (processed_image == image * 2).all()
 
     # try in distributed mode
-    processed_image = frame_by_frame(image_processing_function, use_dask=True)(
-        image
+    processed_image = frame_by_frame(image_processing_function)(
+        image, use_dask=True
     )
     assert processed_image.shape == image.shape
     assert (processed_image == image * 2).all()
@@ -55,9 +55,9 @@ def test_frame_by_frame_labels(create_4D_labels):
     assert (processed_labels[1] == labels[1] + 1).all()
 
     # try in distributed mode
-    processed_labels = frame_by_frame(
-        label_processing_function, use_dask=True
-    )(labels)
+    processed_labels = frame_by_frame(label_processing_function)(
+        labels, use_dask=True
+    )
     assert processed_labels.shape == labels.shape
     assert (processed_labels[0] == labels[0] + 1).all()
     assert (processed_labels[1] == labels[1] + 1).all()
@@ -82,9 +82,9 @@ def test_frame_by_frame_vectors(create_4D_vectors):
     assert (processed_vectors[:, 1, 1:] == vectors[:, 1, 1:] * 2).all()
 
     # try in distributed mode
-    processed_vectors = frame_by_frame(
-        vectors_processing_function, use_dask=True
-    )(vectors)
+    processed_vectors = frame_by_frame(vectors_processing_function)(
+        vectors, use_dask=True
+    )
     assert processed_vectors.shape == vectors.shape
     assert (processed_vectors[:, 0, 1:] == vectors[:, 0, 1:] * 2).all()
     assert (processed_vectors[:, 1, 1:] == vectors[:, 1, 1:] * 2).all()
@@ -101,8 +101,8 @@ def test_frame_by_frame_mesh(create_4d_mesh):
     assert (processed_mesh[1] == mesh[1]).all()
 
     # try in distributed mode
-    processed_mesh = frame_by_frame(mesh_processing_function, use_dask=True)(
-        mesh
+    processed_mesh = frame_by_frame(mesh_processing_function)(
+        mesh, use_dask=True
     )
     assert processed_mesh[0].shape == mesh[0].shape
     assert processed_mesh[1].shape == mesh[1].shape
